36e10c0db69b561146fbc3bc2c88f64651b80cc6
Daniel Therkildsen <dtherkildsen@hotmail.com>
2020-01-14 13:52:16 +0100

    Update stuff

--- app.js
+++ app.js
@@ -7,11 +7,27 @@ app.use(express.static("public"));
 
 io.on("connection", handleSocketConnection);
 
+
 server.listen(process.env.PORT || 3000, handlerServer);
+
+
 
 function handleSocketConnection(socket) {
 	console.log("A user connected");
+	socket.on('disconnect', handleSocketDisconnect);
+	socket.on('userjoin', function handleUserJoin(msg) {
+		io.emit("userjoin", msg)
+	});
+	socket.on('message', function handleChatMessage(msg) {
+		socket.broadcast.emit("message", msg)
+	});
+
 }
+
+function handleSocketDisconnect() {
+	console.log('user disconnected');
+}
+
 
 function handlerServer() {
 	console.log(`Listening on *:${server.address().port}`);
