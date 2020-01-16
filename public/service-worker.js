7e32a9b671e168801e2b0a1602dff82dd1005a62
Daniel Therkildsen <dtherkildsen@hotmail.com>
2020-01-16 09:35:35 +0100

    Update notifications

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,7 +18,7 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v4';
+const CACHE_NAME = 'static-cache-v5';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 // CODELAB: Add list of files to cache here.
@@ -46,57 +46,57 @@ self.addEventListener('install', (evt) => {
 });
 
 self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  self.clients.claim();
+  // console.log('[ServiceWorker] Activate');
+  // // CODELAB: Remove previous cached data from disk.
+  // evt.waitUntil(
+  //   caches.keys().then((keyList) => {
+  //     return Promise.all(keyList.map((key) => {
+  //       if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+  //         console.log('[ServiceWorker] Removing old cache', key);
+  //         return caches.delete(key);
+  //       }
+  //     }));
+  //   })
+  // );
+  // self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
-  if (evt.request.url.includes('/forecast/')) {
-    console.log('[Service Worker] Fetch (data)', evt.request.url);
-    evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(evt.request)
-          .then((response) => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
-            return response;
-          }).catch((err) => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
-          });
-      }));
-    return;
-  }
-  evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request)
-        .then((response) => {
-          return response || fetch(evt.request);
-        });
-    })
-  );
-  evt.respondWith(
-    fetch(evt.request)
-      .catch(() => {
-        return caches.open(CACHE_NAME)
-          .then((cache) => {
-            return cache.match('index.html');
-          });
-      })
-  );
+  // console.log('[ServiceWorker] Fetch', evt.request.url);
+  // // CODELAB: Add fetch event handler here.
+  // if (evt.request.url.includes('/forecast/')) {
+  //   console.log('[Service Worker] Fetch (data)', evt.request.url);
+  //   evt.respondWith(
+  //     caches.open(DATA_CACHE_NAME).then((cache) => {
+  //       return fetch(evt.request)
+  //         .then((response) => {
+  //           // If the response was good, clone it and store it in the cache.
+  //           if (response.status === 200) {
+  //             cache.put(evt.request.url, response.clone());
+  //           }
+  //           return response;
+  //         }).catch((err) => {
+  //           // Network request failed, try to get it from the cache.
+  //           return cache.match(evt.request);
+  //         });
+  //     }));
+  //   return;
+  // }
+  // evt.respondWith(
+  //   caches.open(CACHE_NAME).then((cache) => {
+  //     return cache.match(evt.request)
+  //       .then((response) => {
+  //         return response || fetch(evt.request);
+  //       });
+  //   })
+  // );
+  // evt.respondWith(
+  //   fetch(evt.request)
+  //     .catch(() => {
+  //       return caches.open(CACHE_NAME)
+  //         .then((cache) => {
+  //           return cache.match('index.html');
+  //         });
+  //     })
+  // );
 });
